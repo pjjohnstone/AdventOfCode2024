f670959577030d291574b9013132220137db35fd
Patrick Johnstone <johnstonep@gmail.com>
2024-12-01 10:46:03 +0000

    Initial repo

---
+++ AdventOfCode2024.Tests/Day1.fs
@@ -0,0 +1,11 @@
+﻿module AdventOfCode2024.Tests
+
+open NUnit.Framework
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<Test>]
+let Test1 () =
+    Assert.Pass()
---
+++ AdventOfCode2024/Day1.fs
@@ -0,0 +1,2 @@
+﻿module AdventOfCode2024.Day1
+
